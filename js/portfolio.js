02abed318e5fcc708d5e0202b848ede08053a378
ebruesewitz <brueseen@mail.uc.edu>
2017-03-02 14:45:59 -0500

    working on interactive resume

---
+++ js/portfolio.js
@@ -0,0 +1,15 @@
+function openMe(){
+  document.getElementById('me').style.display="block";
+}
+
+function closeMe(){
+  document.getElementById('me').style.display="none";
+}
+
+function openEducation(){
+  document.getElementById('education').style.display="block";
+}
+
+function closeEducation(){
+  document.getElementById('education').style.display="none";
+}
